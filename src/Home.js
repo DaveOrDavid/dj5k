07ef82ad3efa85f230647bd5f26f11bf632d337f
DaveOrDavid <david.santore@gmail.com>
2019-08-04 12:30:50 -0400

    Home - added simple-flexbox npm package
    Home - added simple-flexbox npm package
    1. npm location: https://www.npmjs.com/package/simple-flexbox
    2. updated from publisher as of 6 days ago when installed.

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,53 @@
 import React, { Component } from 'react'
+import { Column, Row } from 'simple-flexbox'
 import './Home.scss'
 // import Modal from 'react-bootstrap/Modal'
 // import { Link } from 'react-router-dom'
 
 class Home extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      hello: null
-    }
-  }
-
-  componentDidMount () {
-
-  }
+  // constructor (props) {
+  //   super(props)
+  //
+  //   this.state = {
+  //     hello: null
+  //   }
+  // }
+  //
+  // componentDidMount () {
+  //
+  // }
 
   render () {
-    const { hello } = this.state
-
-    // console.log('joke is', joke)
+    // const { hello } = this.state
 
     return (
-      <React.Fragment>
-        <div className="gone">{hello}</div>
-        <section>
-          <h1>Welcome to the Dad Joke 5000!</h1>
-          <h2>Click the Main Menu to get started</h2>
-        </section>
-      </React.Fragment>
+      <Column flexGrow={1}>
+        <Row horizontal='center'>
+          <h1>HEADER</h1>
+        </Row>
+        <Row vertical='center'>
+          <Column flexGrow={1} horizontal='center'>
+            <h3> Column 1 </h3>
+            <span> column 1 content </span>
+          </Column>
+          <Column flexGrow={1} horizontal='center'>
+            <h3> Column 2  </h3>
+            <span> column 2 content </span>
+          </Column>
+        </Row>
+      </Column>
     )
   }
 }
+//       <React.Fragment>
+//         <div className="gone">{hello}</div>
+//         <section>
+//           <h1>Welcome to the Dad Joke 5000!</h1>
+//           <h2>Click the Main Menu to get started</h2>
+//         </section>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
 export default Home
